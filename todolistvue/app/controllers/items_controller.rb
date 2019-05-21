bd6c27a81c465b640b019a728c8ba84ac2b9c0f4
akhdede <akhdede@gmail.com>
2019-05-22 01:51:38 +0800

    first

---
+++ todolistvue/app/controllers/items_controller.rb
@@ -0,0 +1,23 @@
+class ItemsController < ApplicationController
+  def index
+    items = Item.all
+    render json: items
+  end
+
+  def create
+    item = Item.create(resource_params)
+    render json: item
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    render json: item
+  end
+
+  private
+
+  def resource_params
+    params.require(:item).permit(:name)
+  end
+end
